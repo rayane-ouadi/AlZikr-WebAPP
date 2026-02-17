85f466dcbc00ed8d943892b332c222bd356bbfac
Rayane-Ouadi <ouadirayane.off@gmail.com>
2026-02-17 11:56:21 +0100

    1.2

--- Script.js
+++ Script.js
@@ -5,3 +5,28 @@ function dec(btn) {
         span.textContent = current - 1;
     }
 }
+
+function inc(){
+    const numSpan = document.getElementById('NumZikr');
+    const currentB = Number(numSpan.textContent);
+    numSpan.textContent = currentB + 1 ;
+}
+
+let zikrNowCont = document.getElementById('zikrNow');
+const now = new Date();
+let currentHour = now.getHours();
+let link = document.getElementById('link');
+function zikrNowSys(){
+    if(currentHour <= 12){
+        zikrNowCont.textContent = 'Time For Morning Azkar'
+        link.href = "Pages/Morning.html"
+    } else if (currentHour > 12){
+        zikrNowCont.textContent = 'Time For Evening Azkar'
+        link.href = "Pages/Evening.html"
+    } else if(currentHour > 20){
+        zikrNowCont.textContent = 'Time Sleep Azkar'
+        link.href = "Pages/Sleep.html"
+    }
+}
+
+zikrNowSys()
